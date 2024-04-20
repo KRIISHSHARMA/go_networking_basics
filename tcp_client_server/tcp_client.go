58cbdf6b3ad4a2c7ec95e2d115f17b0022d74499
KRIISHSHARMA <kriish.sharma2006@gmail.com>
2024-04-20 12:11:26 +0530

    basic tcp connections using go

---
+++ tcp_client_server/tcp_client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	conn, err := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ReadNWrite(conn)
+	conn.Close()
+}
+
+func ReadNWrite(conn net.Conn) {
+	message := "\nPING PING PING PING PING\n"
+        for i:= 0 ; i < 10 ; i++ {
+
+	    _, write_err := conn.Write([]byte(message))
+
+	   if write_err != nil {
+		fmt.Println("failed:", write_err)
+		return
+            }
+        }
+        conn.(*net.TCPConn).CloseWrite()
+
+	buf, read_err := ioutil.ReadAll(conn)
+	if read_err != nil {
+		fmt.Println("failed:", read_err)
+		return
+	}
+	fmt.Println(string(buf))
+}
+
