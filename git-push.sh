706af30142ace71f2f6f7cbd950337ce7769f950
KRIISHSHARMA <kriish.sharma2006@gmail.com>
2024-04-20 12:19:32 +0530

    basic tcp client server connection in go

--- git-push.sh
+++ git-push.sh
@@ -1,14 +1,17 @@
 echo "Enter your message"
 read message
+
 git add .
 git commit -m"${message}"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "IT IS CLEAN"
 else
- git status
- echo "Enter your github url"
- git remote add origin $(https)
- echo "Pushing data to remote server!!!"
- git push -u origin master
+    git status
+    echo "Enter your GitHub URL"
+    read github_url
+    git remote add origin $github_url
+    echo "Pushing data to the remote server!!!"
+    git push -u origin master
 fi
+
